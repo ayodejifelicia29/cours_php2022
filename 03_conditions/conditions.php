a0a6291687f101405f5ef176a2884a2d67285849
Ayodeji Felicia <felicia.ayodeji55@gmail.com>
2022-06-07 12:21:18 +0200

    Update conditions.php

--- 03_conditions/conditions.php
+++ 03_conditions/conditions.php
@@ -18,28 +18,29 @@
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body class="bg-dark">
-    <!-- JUMBOTRON -->
-    <div class="jumbotron bg-dark text-white text-center">
+     <!-- JUMBOTRON -->
+     <div class="jumbotron  text-center">
         <h1 class="display-3">Cours PHP7 - Les conditions</h1>
         <hr>
-        <p class="lead">On retrouve dans PHP la plupart des instructions de contrôle des scripts. 
-            Indispensables à la gestion du déroulement d'un algorithme quelconque, ces instructions sont présentes dans tous les langages. 
-            PHP utilise une syntaxe très proche de celle du langage C. Ceux qui ont déjà pratiqué un langage tel que le C ou plus simplement
-             JavaScript seront en pays de connaissance.</p>
+        <p class="lead bg-light">On retrouve dans PHP la plupart des instructions de contrôle des scripts. 
+            Indispensables à la gestion du déroulement d'un algorithme quelconque, ces instructions 
+            sont présentes dans tous les langages. PHP utilise une syntaxe très proche de celle du langage C.
+             Ceux qui ont déjà pratiqué un langage tel que le C ou plus simplement JavaScript seront en pays de 
+             connaissance.</p>
     </div>
 
-    <!-- RANGÉE PRINCIPALE -->
-    <div class="row">
-        <!-- LA NAVIGATION EN INCLUDE (penser à ajouter le JS qui va avec en fin de page) -->
+    <!-- RANGEE PRINCIPALE -->
+    <div class="row bg-light">
+        <!-- La barre de navigation en include (penser à toujours ajouter le JS en fin de page)    --->
         <?php
-        require('../inc/sidenav.inc.php');
+        require('../inc/sidenav.inc.php');    
         ?>
-
-        <!-- ============================================================== -->
-        <!-- Contenu principal -->
-        <!-- ============================================================== -->
+     <!-- ============================================================== -->
+     <!-- Contenu principal -->
+     <!-- ============================================================== -->
+     
         <div class="col-sm-8">
-            <main class="container-fluid">
+            <main class="container-fluid bg-light">
                 <!-- BOUTON DE LA NAV -->
                 <button type="button" id="sidebarCollapse" class="navbar-btn">
                 <span></span>
@@ -52,8 +53,10 @@
                     <h2 class="col-sm-12 text-center" id="condtions">1 - Les conditions avec if</h2>
                     <div class="col-sm-12 col-md-4" id="if">
                         <h3 class="text-center">1 - if</h3>
-                        <p>L'instruction <code>if</code> est la plus simple et la plus utilisée des instructions conditionnelles. 
-                        Présente dans tous les langages de programmation, elle est essentielle en ce qu'elle permet d'orienter l'exécution 
+                        <p>L'instruction <code>if</code> est la plus simple et la plus utilisée des instructions
+                         conditionnelles. 
+                        Présente dans tous les langages de programmation, elle est essentielle en ce qu
+                        'elle permet d'orienter l'exécution 
                         du script en fonction de la valeur booléenne d'une expression. </p>
                         <?php 
                             $a = 100;
@@ -62,22 +65,28 @@
                             $d = 8;
 
                             if ($a > $b && $b > $c) {
-                            echo "<p class=\"alert alert-success w-75 mx-auto text-center\">Les deux conditions sont respectées.</p>";
+                            echo "<p class=\"alert alert-success w-75 mx-auto text-center\">Les deux 
+                            conditions sont respectées.</p>";
                             }
                         ?>
                     </div><!-- fin de la colonne -->
 
-                    <div class="col-sm-12 col-md-4">
-                        <h3 class="text-center" id="ifElse">2 - if.. else</h3>
-                        <p>L'instruction <code>if... else</code> permet de traiter le cas où l'expression conditionnelle est 
-                        TRUE et en même temps d'écrire un traitement de rechange quand elle est évaluée à FALSE, 
-                        ce que ne permet pas une instruction if seule. L'instruction ou le bloc qui suit <code>else</code> est alors 
-                        le seul à être exécuté. L'exécution continue alors ensuite normalement après le bloc <code>else</code>.</p>
+                    <div class="col-sm-12 col-md-4"id="ifElse">
+                        <h3 class="text-center">2 - if.. else</h3>
+                        <p>L'instruction <code>if... else</code> permet de traiter le cas où l'expression
+                         conditionnelle est TRUE et en même temps d'écrire un traitement de rechange quand elle 
+                         est évaluée à FALSE,
+                         ce que ne permet pas une instruction if seule. L'instruction ou le bloc qui suit
+                          <code>else</code> est alors 
+                        le seul à être exécuté. L'exécution continue alors ensuite normalement après le bloc
+                         <code>else</code>.</p>
                         <?php 
                             if ($a > $b){
-                                echo "<p class=\"alert alert-success w-75 mx-auto text-center\">$a est supérieur à $b.</p>";
+                                echo "<p class=\"alert alert-success w-75 mx-auto text-center\">$a est supérieur
+                                 à $b.</p>";
                             }else{
-                                echo "<p class=\"alert alert-success w-75 mx-auto text-center\">$b est supérieur à $a</p>";
+                                echo "<p class=\"alert alert-success w-75 mx-auto text-center\">$b est supérieur
+                                 à $a</p>";
                             }
                         ?>
                         <p>Le bloc qui suit les instruction if ou else peut contenir tout sortes d'instructions, 
@@ -85,12 +94,13 @@
                             plus complète de la forme cf.page exo.</p> 
                     </div><!-- fin de la colonne -->
 
-                    <div class="col-sm-12 col-md-4">
-                        <h3 class="text-center" id="ifElseIf">3 - if.. else if.. else</h3>
-                        <p>Nous aurons ici une syntaxe un peu plus compliquée de la forme suivante : <code>if (condition) {} 
-                            else if (condition) else {}</code></p>
-                        <p>Ici, notre variable d est égale à 8. On teste différentes conditions pour voir laquelle est vraie grâce
-                             à un <code>if... else if... else.</code>. Ici c'est la première condition qui est vraie.</p>
+                    <div class="col-sm-12 col-md-4"id="ifElseIfELSE">
+                        <h3 class="text-center" >3 - if.. else if.. else</h3>
+                        <p>Nous aurons ici une syntaxe un peu plus compliquée de la forme suivante : <code> 
+                          if  (condition) {}  else if (condition) {} else {}</code></p>
+                         <p>Ici, notre variable d est égale à 8. On teste différentes conditions pour voir laquelle 
+                         est vraie grâce  à un <code>if... else if... else.</code>. Ici c'est la première condition
+                            qui est vraie.</p>
                         <?php 
                             echo "<p class=\"alert alert-success w-75 mx-auto text-center\">";
                             if ($d == 8){
@@ -100,6 +110,7 @@
                             } else {
                                 echo "Réponse 3 : Les conditions précédentes sont fausses.";
                             }
+                            echo "</p>";
                         ?> 
                         <?php 
                             $e = 10;
@@ -117,13 +128,14 @@
                     <div class="col-sm-12 col-md-6">
                         <h3 class="text-center" id="ternaire">4 - La méthode ternaire</h3>
                         <p>Il existe d'autres façons d'écrire un if else : la méthode ternaire. Avec cette méthode, 
-                            le code sera le suivant : <code>(condition) ? code a exécuter si la condition retourne TRUE : 
-                                code à retourner si le code renvoie FALSE;</code></p>
-                        <?php 
+                            le code sera le suivant : <code>(condition) ? code a exécuter si la condition retourne 
+                                TRUE : code à retourner si le code renvoie FALSE;</code></p>
+                            <?php 
                             $h = 10;
                             // en ternaire 
-                            echo ($h == 10) ?"<p class=\"alert alert-success w-75 mx-auto text-center\">\$h est égal à 10.</p>" 
-                            : "<p class=\"alert alert-success w-75 mx-auto text-center\">\$h est différent de 10</p>";
+                           echo ($h == 10) ?"<p class=\"alert alert-success w-75 mx-auto text-center\">\$h est égal à
+                            10.</p>" 
+                         : "<p class=\"alert alert-success w-75 mx-auto text-center\">\$h est différent de 10</p>";
                         ?> 
                     </div><!-- fin de la colonne -->
 
@@ -141,17 +153,17 @@
                                 case 41:
                                     echo "Loir-et-Cher";
                                     break;
-                                case 92:
-                                    echo "Hauts-de-Seine";
+                                case 91:
+                                    echo "Essonne";
                                     break;
                                 default:
-                                    echo "Département inconnu !";
+                             echo "Département inconnu !!! Revoyez vos cours de Géo la département 100 n'existe pas";
                                     break;
                             }
                             echo "</p>";
                         ?> 
-                    </div><!-- fin de la colonne -->
-                </div><!-- fin de la rangée -->
+                   </div><!-- fin de la colonne -->
+                 </div><!-- fin de la rangée (row)-->
                 <hr>
 
                 <div class="row">
@@ -160,26 +172,29 @@
                         <p>Les boucles permettent de répéter des opérations élémentaires un grand nombre de fois sans 
                             avoir à réécrire le même code. </p>
                     </div>
-
+                    
                     <div class="col-sm-12 col-md-6">
                         <h3 class="text-center">1 - Boucle while</h3>
-                        <p>La boucle <code>while</code> permet d'affiner le comportement d'une boucle en
-                         réalisant une action de manière répétitive tant qu'une condition est vérifiée ou qu'une expression 
-                         quelconque est évaluée à TRUE et donc de l'arrêter quand elle n'est plus vérifiée, évaluée à FALSE.</p>
+                        <p>La boucle <code>while</code> permet d'affiner le comportement d'une boucle en 
+                        réalisant une action de manière répétitive tant qu'une condition est vérifiée ou qu'une 
+                        expression quelconque est évaluée à TRUE et donc de l'arrêter quand elle n'est plus vérifiée,
+                         évaluée à FALSE.</p>
                         <div class="alert alert-success w-75 mx-auto text-center">
                         <?php 
                             $n = 1;
-                            while ($n%7 != 0) { // le cript continue jusqu'à trouver un multiple de 7
-                                $n = rand(1,100); // rand fait un tirage de nombres aléatoires compris entre 1 et 100 rand() pour random
-                                echo $n . "&nbsp; - "; 
+                          while ($n%7 != 0) { // le cript continue jusqu'à trouver un multiple de 7
+                      $n = rand(1,100); // rand fait un tirage de nombres aléatoires compris entre 1 et 100
+                     /*  rand() pour random */
+                             echo $n . "&nbsp; - "; 
                             }
                         ?> 
                         </div>
-                    </div> <!-- fin de la colonne -->
+                 </div> <!-- fin de la colonne -->
 
                     <div class="col-sm-12 col-md-6">
                         <h3 class="text-center" id="dowhile">2 - Boucle do... while</h3>
-                        <p>Avec l'unstruction <code>do ... while</code>, la condition n'est évaluée qu'après une première exécution
+                        <p>Avec l'unstruction <code>do ... while</code>, la condition n'est évaluée
+                         qu'après une première exécution
                          des instructions du bloc compris entre do et while. </p>
                         <div class="alert alert-success w-75 mx-auto text-center">
                         <?php 
@@ -191,10 +206,12 @@
                         ?> 
                         </div>
                     </div> <!-- fin de la colonne -->
+                    
                     <div class="col-sm-12 col-md-6">
                         <h3 class="text-center">3 - Boucle for</h3>
                         <p>La boucle <code>for</code> est plus concise, ramassée que la boucle <code>while</code>. 
-                        Elle s'écrit ainsi : <code>for(iniatialisation; condition; incrémentation){ code à exécuter }</code></p>
+                        Elle s'écrit ainsi : <code>for(iniatialisation; condition; incrémentation)
+                            { code à exécuter }</code></p>
                         <div class="alert alert-success w-75 mx-auto text-center">
                         <?php 
                             //on va afficher les puissances de 2 jusqu'à 8
@@ -208,7 +225,8 @@
 
                     <div class="col-sm-12 col-md-6">
                         <h3 class="text-center">4 - Boucle foreach</h3>
-                        <p>La boucle <code>foreach</code> (pour chaque passage), est efficace pour afficher et lister 
+                        <p>La boucle <code>foreach</code> (pour chaque passage), est efficace pour afficher 
+                        et lister 
                         les éléments contenus dans un tableau. </p>
                         <div class="alert alert-success w-75 mx-auto text-center">
                         <?php 
@@ -222,7 +240,8 @@
                     </div> <!-- fin de la colonne -->
 
                     <div class="col-sm-12">
-                        <p class="alert alert-success w-50 mx-auto text-center">Lecture des indices et des valeurs d'un tableau :<br>
+                     <p class="alert alert-success w-50 mx-auto text-center">Lecture des indices et des valeurs 
+                            d'un tableau :<br>
                         <?php 
                             // crétion d'un autre tableau avec une boucle for
                             for ($i = 0; $i <= 8; $i++) {
