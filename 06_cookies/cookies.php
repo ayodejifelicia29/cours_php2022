3c356ab944964f37c22655e26b340f576427c588
Ayodeji Felicia <felicia.ayodeji55@gmail.com>
2022-06-13 12:19:34 +0200

    Create cookies.php

---
+++ 06_cookies/cookies.php
@@ -0,0 +1,75 @@
+<?php
+require_once('../inc/functions.php')
+ // 2- si une langue est passée dans l'url (l'internaute a cliqué sur un lien), on enverra cette langue dans le cookie. 
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <!-- font google -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap" rel="stylesheet">
+
+    <title>Cours_PHP2022- PDO</title>
+
+    <!-- mes styles -->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body class="bg-light">
+    <!-- JUMBOTRON -->
+    <div class="jumbotron bg-dark text-white text-center">
+        <h1 class="display-3">Cours_PHP2022 - PDO </h1>
+        <hr>
+        <p class="lead ">La variable "$pdo" est un objet qui représente la connexion à une BDD.</p>
+    </div>
+               
+    <!-- RANGEE PRINCIPALE -->
+    <div class="row bg-light">
+        <!-- La barre de navigation en include (penser à toujours ajouter le JS en fin de page)    --->
+        <?php
+        require('../inc/sidenav.inc.php')
+        ?>
+        <!-- ============================================================== -->
+        <!-- Contenu principal -->
+        <div class="col-sm-8">
+            <main class="container-fuild">
+                <div class="row">
+                    <hr>
+                    <h2 class="col-sm-12 text-center" id="definition">1 - Connexion à la BDD</h2>
+                    <div class="col-sm-12">
+                    </div>
+                 </div>
+           
+           </main>
+           </div><!-- FIN DE LA CONTENU PRINCIPALE col-8  -->
+        
+
+
+
+    
+        </div> <!-- FIN DU RANGEE PRINCIPAL (row) -->
+   
+    <!-- LE FOOTER EN REQUIRE -->
+    <?php
+    require("../inc/footer.inc.php")
+    ?>
+
+    <!-- Optional JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+
+    <!-- le js pour la navigation  -->
+    <script src="../inc/sidenav.js"></script>
+
+</body>
+
+</html>
