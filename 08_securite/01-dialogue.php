dbfe993570dae94bfc64607ddc87b337687fe7d0
Ayodeji Felicia <felicia.ayodeji55@gmail.com>
2022-06-17 13:18:15 +0200

    up

--- 08_securite/01-dialogue.php
+++ 08_securite/01-dialogue.php
@@ -77,7 +77,7 @@
                             //connexion à la base de données dialogue
                             $pdoDialogue = new PDO('mysql:host=localhost;dbname=dialogue',
                             'root',
-                            '',
+                            'root',
                             array(
                                 PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
                                 PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
