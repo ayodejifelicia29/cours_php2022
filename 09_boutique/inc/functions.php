200283bd23e39d1af93d3173da35833b6b9a05a9
Ayodeji Felicia <felicia.ayodeji55@gmail.com>
2022-06-20 16:06:31 +0200

    Update functions.php

--- 09_boutique/inc/functions.php
+++ 09_boutique/inc/functions.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< Updated upstream
 //1-fonction print-r
 function jeprint_r($mavariable){
     echo "<small class=\"bg-primary text-white p-2\">print_r :</small><pre class=\"alert alert-primary w-50\">";
@@ -21,5 +22,7 @@ function executeRequete($requete, $parametres = array()) {  // utile pour toutes
         }
 }//fermeture de la fonction executeRequete
  
+=======
+>>>>>>> Stashed changes
 
 ?>
