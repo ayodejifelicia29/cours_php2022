a87879bbd2578f3a19539708198728c882f230f6
Ayodeji Felicia <felicia.ayodeji55@gmail.com>
2022-06-21 15:08:10 +0200

    cr

--- 09_boutique/inc/functions.php
+++ 09_boutique/inc/functions.php
@@ -1,24 +1,26 @@
 <?php
-//1-fonction print-r
-function jeprint_r($mavariable){
+
+//// 1- fonction print_r //////
+function jeprint_r($mavariable)
+{
     echo "<small class=\"bg-primary text-white p-2\">print_r :</small><pre class=\"alert alert-primary w-50\">";
     print_r($mavariable);
     echo "</pre>";
 }
 
-////2- Fonction pour exécuter les prepare()////
-function executeRequete($requete, $parametres = array()) {  // utile pour toutes les requêtes 1/ la requête 2/ fabrication du tableau avec les marqueurs
-    foreach ($parametres as $indice => $valeur) { // boucle foreach
-        $parametres[$indice] = htmlspecialchars($valeur); // opur éviter les injections SQL
-        global $pdoSITE;// "global"  va nous permet d'accéder à la variable $pdoSITE et de dire qu'elle devient global du fichier init.inc.php
-        $resultat=$pdoSITE->prepare($requete);//puis on prepare la requete
-        $succes =$resultat->execute($parametres); //Et on éxecute celle-ci
-        if($succes === false){
-            return false; //Si la requete ne s'execute pas
-        }else{
+//// 2- fonction pour exécuter les prepare()////
+function executeRequete($requete, $parametres = array())
+{
+    foreach ($parametres as $indice => $valeur) {
+        $parametres[$indice] = htmlspecialchars($valeur);  //on évite les injections SQL
+        global $pdoSITE; // global va nous permettre d'accéder à la variable $pdoSITE et de dire qu'elle devient globale
+        $resultat = $pdoSITE->prepare($requete); //puis on prépare la requête
+        $succes = $resultat->execute($parametres); // Et on éxecute celle-ci
+        if ($succes === false) {
+            return false; //Si la requête ne s'execute pas
+        } else {
             return $resultat;
-        }//fin if else
-        }
-}//fermeture de la fonction executeRequete
-
+        } // fin if else
+    }
+} // fermeture de la fonction executeRequete
 ?>
--- 09_boutique/inc/init.php
+++ 09_boutique/inc/init.php
@@ -1,30 +1,31 @@
 <?php
+    
+// fichier indispensable au fonctionnement du site
 
-   //fichier indispensable au fonctionnement du site
-
-   /////////////////////////////////////////////CONNEXION A LA BDD//////////////
-
-   ///1-Connexion
-   $pdoSITE = new PDO('mysql:host=localhost;dbname=site',
-   'root',
-   'root',
-   array(
-     PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,// pour afficher les warnings SQL dans le navigateur
-     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',// pour définir le charset des échanges avec la BDD
-   ));
-
-   //////2- Ouverture de session
-   session_start();
-
-   /////3-chemin du site avec une constante
+///////////////////////////////////////////////
+ ///////////// CONNEXION À LA BDD //////////////
+    ///////////////////////////////////////////////
 
 
+/////// 1- Connexion
 
-   //// 4-variable pour les contenus
-   $contenu ='';
+$pdoSITE = new PDO('mysql:host=localhost;dbname=site',
+'root',
+'root',
+array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',   
+));
 
-   ///5- inclusion des fonctions
+///// 2- Ouverture de session
+session_start();
 
-   require_once('functions.php');
+//// 3- chemin du site avec une constante
 
-?>
+//// 4 - variable pour les contenus
+$contenu = '';
+
+//// 5 - inclusion des fonctions
+require_once('functions.php');
+?>  
+
